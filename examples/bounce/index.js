87fb3a8662f21cad6afacb52decff884a0b0f287
Cedric Stoquer <cstoquer@wizcorp.jp>
2015-09-18 17:29:14 +0900

    common Sprite script and code cleanup

--- examples/bounce/index.js
+++ examples/bounce/index.js
@@ -1,9 +1,10 @@
 var image   = document.getElementById('image');
 var canvas  = document.getElementById('canvas');
 var ctx     = canvas.getContext('2d');
-var sprite  = null;
+var sprite  = new Sprite(image);
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+
 function createAnimation(x, y) {
 	var duration = 1500;
 	var easingParameter = 4;
@@ -23,29 +24,6 @@ function createAnimation(x, y) {
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 
-function Sprite() {
-	this.x        = 0;
-	this.y        = 0;
-	this.pivotX   = image.width  / 2;
-	this.pivotY   = image.height / 2;
-	this.scale    = 1;
-	this.rotation = 0;
-	this.alpha    = 1;
-}
-
-Sprite.prototype.draw = function (ctx) {
-	ctx.save();
-	ctx.globalAlpha *= this.alpha;
-	ctx.translate(this.x , this.y);
-	ctx.rotate(this.rotation);
-	ctx.scale(this.scale, this.scale);
-	ctx.translate(-this.pivotX, -this.pivotY);
-	ctx.drawImage(image, 0, 0);
-	ctx.restore();
-};
-
-sprite = new Sprite();
-
 TINA.onUpdate(function update(t, dt) {
 	// At this point, all my tweens are up to date for the current iteration
 	ctx.clearRect(0, 0, 400, 400);
@@ -53,4 +31,3 @@ TINA.onUpdate(function update(t, dt) {
 });
 
 createAnimation();
-
--- examples/particle/index.js
+++ examples/particle/index.js
@@ -47,29 +47,6 @@ canvas.addEventListener('mousedown', function onTap(e) {
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 
-function Sprite() {
-	this.x        = 0;
-	this.y        = 0;
-	this.pivotX   = image.width  / 2;
-	this.pivotY   = image.height / 2;
-	this.scale    = 1;
-	this.rotation = 0;
-	this.alpha    = 1;
-}
-
-Sprite.prototype.draw = function (ctx) {
-	ctx.save();
-	ctx.globalAlpha *= this.alpha;
-	ctx.translate(this.x , this.y);
-	ctx.rotate(this.rotation);
-	ctx.scale(this.scale, this.scale);
-	ctx.translate(-this.pivotX, -this.pivotY);
-	ctx.drawImage(image, 0, 0);
-	ctx.restore();
-};
-
-//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-
 for (var i = 0; i < 100; i++) {
-	sprites.push(new Sprite());
+	sprites.push(new Sprite(image));
 }
