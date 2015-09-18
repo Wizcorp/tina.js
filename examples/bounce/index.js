4d27a983dfe6e87cf07a052f5e287b6d0fa4aa62
Cedric Stoquer <cstoquer@wizcorp.jp>
2015-09-18 17:37:06 +0900

    simplier code

--- examples/bounce/index.js
+++ examples/bounce/index.js
@@ -5,29 +5,25 @@ var sprite  = new Sprite(image);
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 
-function createAnimation(x, y) {
-	var duration = 1500;
-	var easingParameter = 4;
-
-	TINA.Tween(sprite, ['x', 'rotation'])
-		.from({ x:   0, rotation: 0 })
-		.to({   x: 500, rotation: 9 }, duration)
-		.iterations(Infinity)
-		.start();
-
-	TINA.Tween(sprite, ['y'])
-		.from({ y: -100 })
-		.to({   y:  300 }, duration, TINA.easing.bounceOut, easingParameter)
-		.iterations(Infinity)
-		.start();
-}
-
-//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-
 TINA.onUpdate(function update(t, dt) {
 	// At this point, all my tweens are up to date for the current iteration
 	ctx.clearRect(0, 0, 400, 400);
 	sprite.draw(ctx);
 });
 
-createAnimation();
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+
+var duration = 2000;
+var bouncing = 4;
+
+TINA.Tween(sprite, ['x', 'rotation'])
+	.from({ x:   0, rotation: 0 })
+	.to({   x: 500, rotation: 9 }, duration)
+	.iterations(Infinity) // loop forever
+	.start();
+
+TINA.Tween(sprite, ['y'])
+	.from({ y: -100 })
+	.to({   y:  300 }, duration, TINA.easing.bounceOut, bouncing) // using bounce easing for y position
+	.iterations(Infinity) // loop forever
+	.start();
